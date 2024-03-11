245c17a1a312224f60301e57996f28c087a47f45
tonny187 <tonyoanthony@gmail.com>
2024-03-11 16:28:57 -0500

    adding a debugger file

---
+++ dd/index.php
@@ -0,0 +1,50 @@
+<?php
+
+require("vendor/autoload.php");
+
+class Michi 
+{
+    protected $patas = [];
+    protected $color;
+
+    function __construct(string $color)
+    {
+        $this->color = $color;
+
+        for ($i=0; $i < 4 ; $i++) 
+        { 
+            $this->patas[$i] = new PataDeMichi();
+        }
+    }
+
+}
+
+class PataDeMichi{
+
+    protected $nails = 4;
+    protected $description = "Tiene manchitas";
+
+    public function get_description(): string
+    {
+        return $this->description;
+    }
+
+}
+
+$casa_de_michis = array(
+    "nombre" => "Michilango",
+    "ubicacion"=> [
+        "pais"=> "Mexico",
+        "ciudad"=>"Ciudad de Mexico",
+        "colonia"=> "Doctores",
+        "numero"=> 27
+    ],
+    "numero_de_michis"=> 3,
+    "michis"=>[
+        new Michi("blanco"),
+        new Michi("negro"),
+        new Michi("gris")
+    ]
+);
+
+dd($casa_de_michis);
