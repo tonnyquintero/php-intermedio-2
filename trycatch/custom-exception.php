deb5efe5f605b597f0ae2f6149e5ecb98bf058c5
tonny187 <tonyoanthony@gmail.com>
2024-03-12 18:49:39 -0500

    adding exception file

---
+++ trycatch/custom-exception.php
@@ -0,0 +1,36 @@
+<?php
+
+class MichiException extends Exception{
+    public function getMeow(){
+        return "Meow";
+    }
+}
+
+class ConejiException extends Exception{
+    public function getRabbut(){
+        return "Conejo";
+    }
+}
+
+try {
+    $exception = readline("Excepcion a lanzar: ");
+    if ($exception == "michi") 
+        throw new MichiException("Meow");
+     else 
+        throw new ConejiException("Conejo");
+    
+
+
+    
+} catch (MichiException $e) {
+   echo $e->getMessage() . "\n";
+   echo $e->getMeow();
+}catch (ConejiException $e) {
+   echo $e->getMessage() . "\n";
+   echo $e->getRabbut();
+}finally{
+    echo "Se ha lanzado la excepcion";
+}
+
+
+?>
