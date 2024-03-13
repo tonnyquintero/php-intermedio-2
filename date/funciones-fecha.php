0d08e7c7baf59bcd0696206c0ba697af2e059412
tonny187 <tonyoanthony@gmail.com>
2024-03-13 05:53:15 -0500

    adding date files

--- date/funciones-fecha.php
+++ date/funciones-fecha.php
@@ -19,3 +19,24 @@ echo date("Y-m-d H:i:s", $unix_time_last_wednesday);
 
 //Fechas inmutables
 $date_inmutable = new DateTimeImmutable();
+echo "\n";
+
+// Diferencia de tiempo
+$today = new DateTime();
+$tonny_birth = new DateTime("1991-03-20");
+$interval = $tonny_birth->diff($today);
+echo $interval->format("%y años con %d días");
+echo "\n";
+
+
+// Crear desde un formato dado
+// $date = DateTime::createFromFormat("l j F y", "Sunday 17 April 2022");
+// echo $date->format("Y-m-d H:i:s");
+// echo "\n";
+
+
+//Modificar una fecha
+$fecha = new DateTime();
+$fecha->modify("+1 day");
+echo $fecha->format("Y-m-d H:i:s");
+echo "\n";
